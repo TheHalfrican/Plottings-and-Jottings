d495254efe561fda2c3a4ff3038e9f10a32bd18d
The Halfrican <noahmattocks@gmail.com>
2022-03-29 20:34:09 -0700

    Wrote Get routes

---
+++ server.js
@@ -0,0 +1,39 @@
+const api = require('./Develop/public/assets/js/index.js');
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+// Middleware
+app.use('/api', api);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// Get routes
+app.get('/', (req, res) => 
+
+res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+
+);
+
+app.get('/notes', (req, res) => 
+
+res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
+
+);
+
+app.get('*', (req, res) =>
+
+res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+
+);
+
+app.listen(PORT, () =>
+
+console.log('App is listening at http://localhost:${PORT}')
+
+);
